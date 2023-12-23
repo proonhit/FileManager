cc9dceabd7a5fad416739b826ab8e3942b6767d8
Duong Nguyen Dau <duongnguyendau@gmail.com>
2023-12-23 10:35:42 +0700

    add rename form

---
+++ ManagerFile/RenameForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ManagerFile
+{
+    partial class RenameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRename = new System.Windows.Forms.TextBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên file/folder";
+            // 
+            // txtRename
+            // 
+            this.txtRename.Location = new System.Drawing.Point(91, 36);
+            this.txtRename.Name = "txtRename";
+            this.txtRename.Size = new System.Drawing.Size(240, 20);
+            this.txtRename.TabIndex = 1;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(80, 96);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(83, 36);
+            this.btnRename.TabIndex = 2;
+            this.btnRename.Text = "Đổi tên";
+            this.btnRename.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Hủy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // RenameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 161);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.txtRename);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RenameForm";
+            this.Text = "Đổi tên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRename;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button button1;
+    }
+}
